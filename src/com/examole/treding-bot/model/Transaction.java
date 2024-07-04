bc1662a34d33c73fa21aa6a59bc4b67dad106128
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-07-04 21:25:19 +0530

    Update Transaction.java

--- src/com/examole/treding-bot/model/Transaction.java
+++ src/com/examole/treding-bot/model/Transaction.java
@@ -2,15 +2,27 @@ package com.example.tradingbot.model;
 
 public class Transaction {
     private String token;
+    private String signature;
+    private long timestamp;
     private boolean processed;
 
-    public Transaction(String token, boolean processed) {
+    public Transaction(String token, String signature, long timestamp, boolean processed) {
         this.token = token;
+        this.signature = signature;
+        this.timestamp = timestamp;
         this.processed = processed;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
     }
 
     public boolean isProcessed() {
@@ -19,10 +31,5 @@ public class Transaction {
 
     public void setProcessed(boolean processed) {
         this.processed = processed;
-    }
-}
-
-    public boolean isBoughtHero() {
-        return boughtHero;
     }
 }
