ac9d63c03fd460c3eea37c0e6cfaf934bf22586c
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-07-04 16:27:11 +0530

    Update App.java

--- src/main/java/com/example/tredingbot/App.java
+++ src/main/java/com/example/tredingbot/App.java
@@ -1,1 +1,12 @@
+package com.example.tradingbot;
+
+import com.example.tradingbot.service.TradingService;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Trading Bot is running!");
+        TradingService tradingService = new TradingService();
+        tradingService.startBot();
+    }
+}
 
